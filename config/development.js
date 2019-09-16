1d230425d740fc62259fccfb6946af805b00512b
Christian Angelone <christiangelone@gmail.com>
2019-09-15 21:39:50 -0300

    Initial commit

--- config/development.js
+++ config/development.js
@@ -1,5 +1,6 @@
 module.exports = {
     ENV: process.env.NODE_ENV,
     DB_URI: 'postgres://postgres@db:5432/myhealthapp',
-    DB_DIALECT: 'postgres'
+    DB_DIALECT: 'postgres',
+    PORT: 3333
 }
--- config/production.js
+++ config/production.js
@@ -1,5 +1,6 @@
 module.exports = {
     ENV: process.env['NODE_ENV'],
     DB_URI: process.env['DATABASE_URL'],
-    DB_DIALECT: 'postgres'
+    DB_DIALECT: 'postgres',
+    PORT: process.env.PORT
 }
