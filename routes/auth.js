f807b3b31f5779a3913c278ed194a9decb06f3d5
Christian Angelone <christiangelone@gmail.com>
2019-09-15 22:49:44 -0300

    Initial commit

--- routes/auth.js
+++ routes/auth.js
@@ -11,8 +11,9 @@ router.post('/login', function (req, res) {
         if (!user) return res.json({ error: "DNI y/o password incorrecto"})
         if (password !== user.hashed_password) return res.json({ error: "DNI y/o password incorrecto"})
         
+        delete user.hashed_password
         const token = jwt.sign({ idn, role }, secret, )
-        return res.json({ token })
+        return res.json({ user, token })
     })
 })
 
